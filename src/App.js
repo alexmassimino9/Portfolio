3965a9beace3c58313c4f50a6fa7e90a86ba51b5
alexmassimino9 <alexmassimino9@gmail.com>
2022-07-15 22:28:15 -0400

    update default route

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
     <>
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
