2b30485ae058b95ad9978f038a0caa6797cf637b
alexmassimino9 <alexmassimino9@gmail.com>
2022-07-18 23:29:56 -0400

    add routes folder and config imports

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 import Navbar from "./components/Navigation/navbar";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Home from "./components/home";
-import Projects from "./components/projects";
-import Contact from "./components/contact";
+import Home from "./routes/home";
+import Projects from "./routes/projects";
+import Contact from "./routes/contact";
 import Footer from "./components/footer";
 
 const App = () => {
