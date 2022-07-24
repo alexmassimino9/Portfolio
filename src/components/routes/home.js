64c12740203dc0d1c0de830a5157d9b2b7a8651f
alexmassimino9 <alexmassimino9@gmail.com>
2022-07-23 21:12:47 -0400

    fix resume button not working

--- src/components/routes/home.js
+++ src/components/routes/home.js
@@ -1,6 +1,7 @@
 import { Box } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import React from "react";
+import resume from "../../assets/Amassimino.png";
 
 const Home = () => {
   let navigate = useNavigate();
@@ -32,7 +33,7 @@ const Home = () => {
             projects
           </button>
           <button className="btn">
-            <a id="resume" href="../assets/Amassimino.png" download>
+            <a id="resume" href={resume} download>
               resume
             </a>
           </button>
