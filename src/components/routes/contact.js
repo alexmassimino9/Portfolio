0141a172d609c35128c9ff3da35e89160b1eed7e
alexmassimino9 <alexmassimino9@gmail.com>
2022-07-26 02:57:33 -0400

    add contact form functionality

--- src/components/routes/contact.js
+++ src/components/routes/contact.js
@@ -1,17 +1,41 @@
-import { Container } from "@mui/system";
-import React from "react";
+import emailjs from "@emailjs/browser";
+import React, { useRef } from "react";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_s6q61tt",
+        "contact_form",
+        form.current,
+        "v5s6sK-NINaZ_1wli"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          e.target.reset();
+          alert("Email Sent");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
-    <Container
-      fixed
-      sx={{
-        bgcolor: "lightgrey",
-        height: "100vh",
-      }}
-    >
-      <h2>This is going to be the contact page</h2>
-    </Container>
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
   );
 };
 
