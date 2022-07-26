dbdd041df38a3388dbc71fd5df2d241d37ab0f37
alexmassimino9 <alexmassimino9@gmail.com>
2022-07-26 05:30:34 -0400

    add styles to contact form

--- src/components/routes/contact.js
+++ src/components/routes/contact.js
@@ -1,5 +1,7 @@
 import emailjs from "@emailjs/browser";
 import React, { useRef } from "react";
+import { Container } from "@mui/system";
+import { Button, TextField, Typography } from "@mui/material";
 
 const Contact = () => {
   const form = useRef();
@@ -27,15 +29,58 @@ const Contact = () => {
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+    <Container
+      sx={{
+        border: "1px solid red",
+        padding: "100px",
+        height: "100vh",
+      }}
+    >
+      <Typography
+        variant="h3"
+        component="div"
+        mb={10}
+        fontFamily="'Edu VIC WA NT Beginner', cursive"
+      >
+        Send me a message:
+      </Typography>
+      <form ref={form} onSubmit={sendEmail} className="form">
+        <TextField
+          margin="dense"
+          required
+          label="Name: "
+          type="text"
+          name="user_name"
+          className="form-control"
+        />
+        <TextField
+          margin="dense"
+          required
+          label="Email: "
+          type="email"
+          name="user_email"
+          className="form-control"
+        />
+        <TextField
+          margin="dense"
+          required
+          label="Message: "
+          type="text"
+          multiline
+          rows={4}
+          name="message"
+          className="form-control"
+        />
+        <Button
+          type="submit"
+          value="Send"
+          variant="contained"
+          className="form-control"
+        >
+          Submit
+        </Button>
+      </form>
+    </Container>
   );
 };
 
