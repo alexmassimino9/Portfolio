933836d04f277f4268982db4c0b040aef2955c81
alexmassimino9 <alexmassimino9@gmail.com>
2022-08-07 22:27:51 -0400

    Updates

---
+++ precache-manifest.bb085e9e46ee2699b0ce3e7d4fd88e93.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5141f1de8069a015164",
+    "url": "/portfolio/static/css/main.98fd6ab2.chunk.css"
+  },
+  {
+    "revision": "b5141f1de8069a015164",
+    "url": "/portfolio/static/js/main.b5141f1d.chunk.js"
+  },
+  {
+    "revision": "2e081abecca541cd8074",
+    "url": "/portfolio/static/js/1.2e081abe.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "0d079f5992f3f06497c09229a6a34b03",
+    "url": "/portfolio/static/media/Amassimino.0d079f59.png"
+  },
+  {
+    "revision": "440b9b231a84584c31de471bd31d0985",
+    "url": "/portfolio/static/media/default-image.440b9b23.png"
+  },
+  {
+    "revision": "bb8aa2bf5f8f7fa84394c662a87ee537",
+    "url": "/portfolio/static/media/Portfolio.bb8aa2bf.png"
+  },
+  {
+    "revision": "efd560a87ec92da68d0b4ca3627c2fe8",
+    "url": "/portfolio/index.html"
+  }
+];
