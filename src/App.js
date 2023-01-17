bbad42c31461770654a9dd271bfdcfdae10b2a49
alexmassimino9 <alexmassimino9@gmail.com>
2023-01-16 22:35:40 -0500

    create router routes for new pages

--- src/App.js
+++ src/App.js
@@ -7,14 +7,19 @@ import Projects from "./components/routes/projects";
 import Contact from "./components/routes/contact";
 import Footer from "./components/footer";
 
+// v2 routes
+import { HomePage, ProjectsPage, ContactPage } from "./pages";
 const App = () => {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/portfolio/" element={<Home />} />
+        {/* <Route path="/portfolio/" element={<Home />} />
         <Route path="/portfolio/contact" element={<Contact />} />
-        <Route path="/portfolio/projects" element={<Projects />} />
+        <Route path="/portfolio/projects" element={<Projects />} /> */}
+        <Route path="/portfolio/" element={<HomePage />} />
+        <Route path="/portfolio/contact" element={<ContactPage />} />
+        <Route path="/portfolio/projects" element={<ProjectsPage />} />
       </Routes>
       <Footer />
     </>
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import { ContactPage } from "./contact/";
+import { HomePage } from "./home";
+import { ProjectsPage } from "./projects";
+
+export { ContactPage, HomePage, ProjectsPage };
