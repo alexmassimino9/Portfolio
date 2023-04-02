c00347c53818a8590016888d5a13738339d40576
alexmassimino9 <alexmassimino9@gmail.com>
2023-04-02 12:51:41 -0400

    update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,61 +1,79 @@
 import React from "react";
-import { IconButton } from "@mui/material";
+import { IconButton, Box, Typography } from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import InstagramIcon from "@mui/icons-material/Instagram";
+import EmailIcon from "@mui/icons-material/Email";
 
 const Footer = () => {
   return (
-    <section
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
+    <Box
+      sx={{
         backgroundColor: "#2c2f33",
         color: "white",
-        height: "15vh",
-        width: "100%",
+        py: 4,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
       }}
     >
-      <IconButton
-        href="https://github.com/alexmassimino9"
-        target="_blank"
-        rel="noreferrer noopener"
-        sx={{ color: "white", fontSize: "45px", marginRight: "20px" }}
-      >
-        <GitHubIcon />
-      </IconButton>
-      <IconButton
-        href="https://www.linkedin.com/in/alexmassimino9/"
-        target="_blank"
-        rel="noreferrer noopener"
-        sx={{ color: "white", fontSize: "45px", marginRight: "20px" }}
-      >
-        <LinkedInIcon />
-      </IconButton>
-      <IconButton
-        href="https://www.instagram.com/minocodes/"
-        target="_blank"
-        rel="noreferrer noopener"
-        sx={{ color: "white", fontSize: "45px", marginRight: "20px" }}
-      >
-        <InstagramIcon />
-      </IconButton>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          fontSize: "10px",
-        }}
-      >
-        <span> © Alexander Massimino 2022 </span>
-        <span style={{ marginTop: "5px" }}>
+      <Box sx={{ display: "flex", alignItems: "center" }}>
+        <IconButton
+          href="https://github.com/alexmassimino9"
+          target="_blank"
+          rel="noreferrer noopener"
+          sx={{
+            color: "white",
+            fontSize: "64px",
+            marginRight: "20px",
+            "&:hover": {
+              color: "#8B8B8B",
+            },
+          }}
+        >
+          <GitHubIcon />
+        </IconButton>
+        <IconButton
+          href="https://www.linkedin.com/in/alexandermassimino/"
+          target="_blank"
+          rel="noreferrer noopener"
+          sx={{
+            color: "white",
+            fontSize: "64px",
+            marginRight: "20px",
+            "&:hover": {
+              color: "#8B8B8B",
+            },
+          }}
+        >
+          <LinkedInIcon />
+        </IconButton>
+        <IconButton
+          // prompt user to send email to alexmassimino9@gmail.com
+          href="mailto:alexmassimino9@gmail.com"
+          target="_blank"
+          rel="noreferrer noopener"
+          sx={{
+            color: "white",
+            fontSize: "64px",
+            marginRight: "20px",
+            "&:hover": {
+              color: "#8B8B8B",
+            },
+          }}
+        >
+          <EmailIcon />
+        </IconButton>
+      </Box>
+      <Box sx={{ mt: 4 }}>
+        <Typography variant="subtitle1" sx={{ fontSize: "20px" }}>
+          © Alexander Massimino 2022
+        </Typography>
+        <Typography variant="caption" sx={{ fontSize: "12px", mt: 1 }}>
           created by Alexander Massimino - all rights reserved
-        </span>
-      </div>
-    </section>
+        </Typography>
+      </Box>
+    </Box>
   );
 };
 
