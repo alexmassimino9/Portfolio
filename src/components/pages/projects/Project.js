0722f35c2a8085b9e32d76721c8c1ae475ffd0b6
alexmassimino9 <alexmassimino9@gmail.com>
2023-04-23 00:51:55 -0400

    update project styling

--- src/components/pages/projects/Project.js
+++ src/components/pages/projects/Project.js
@@ -1,13 +1,12 @@
-import React from "react";
 import {
-  Grid,
+  Button,
   Card,
+  CardActions,
   CardContent,
   CardMedia,
-  Typography,
-  Button,
+  Grid,
   Stack,
-  CardActions,
+  Typography,
 } from "@mui/material";
 
 const Project = ({ project }) => {
@@ -20,49 +19,59 @@ const Project = ({ project }) => {
     <Grid item xs={12} sm={6} md={4}>
       <Card
         sx={{
-          height: "100%",
           display: "flex",
           flexDirection: "column",
+          borderRadius: "16px",
           boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+          overflow: "hidden",
           transition: "transform 0.2s ease-in-out",
           "&:hover": {
             transform: "scale(1.05)",
+            boxShadow: "0px 8px 16px rgba(0, 0, 0, 0.2)",
           },
+          height: "100%",
         }}
       >
         <CardMedia
           component="img"
-          height="300"
+          height="200"
           image={img}
           alt={name}
-          sx={{ objectFit: "cover" }}
+          sx={{ objectFit: "cover", objectPosition: "center" }}
         />
         <CardContent
           sx={{
             flexGrow: 1,
-            padding: "1.5rem",
-            height: "180px",
-            overflow: "auto",
+            padding: "1rem",
+            display: "flex",
+            flexDirection: "column",
           }}
         >
           <Typography
             variant="h5"
             component="h2"
-            sx={{ mb: "1rem", fontFamily: "Montserrat", fontWeight: 600 }}
+            sx={{
+              mb: "0.5rem",
+              fontFamily: "Montserrat",
+              fontWeight: 600,
+              color: "#333",
+            }}
           >
             {name}
           </Typography>
           <Typography
             variant="body1"
-            sx={{ mb: "1.5rem", fontFamily: "Roboto", fontWeight: 300 }}
+            sx={{
+              mb: "1rem",
+              fontFamily: "Roboto",
+              fontWeight: 300,
+              color: "#666",
+              flex: "1",
+            }}
           >
             {bio}
           </Typography>
-          <Stack
-            direction="row"
-            spacing={1}
-            sx={{ mt: "1.5rem", flexWrap: "wrap" }}
-          >
+          <Stack direction="row" spacing={1} sx={{ flexWrap: "wrap" }}>
             {tech.split(",").map((t, index) => (
               <Typography
                 key={index}
@@ -82,15 +91,22 @@ const Project = ({ project }) => {
             ))}
           </Stack>
         </CardContent>
-        <CardActions sx={{ justifyContent: "flex-end", padding: "1.5rem" }}>
+        <CardActions
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            padding: "1rem",
+            backgroundColor: "#F7F7F7",
+          }}
+        >
           <Button
             size="large"
             variant="contained"
             onClick={handleViewDemoClick}
             sx={{
-              mr: "1rem",
               fontFamily: "Roboto",
-              fontWeight: 400,
+              fontWeight: 500,
               backgroundColor: "#2ECC71",
               color: "white",
               "&:hover": { backgroundColor: "#27AE60" },
@@ -104,7 +120,7 @@ const Project = ({ project }) => {
             onClick={handleViewCodeClick}
             sx={{
               fontFamily: "Roboto",
-              fontWeight: 400,
+              fontWeight: 500,
               color: "#2ECC71",
               borderColor: "#2ECC71",
             }}
--- src/components/pages/projects/Projects.js
+++ src/components/pages/projects/Projects.js
@@ -1,7 +1,12 @@
 import React, { useEffect } from "react";
-import { Container, Grid, Typography } from "@mui/material";
+import { Container, Grid, Typography, Box } from "@mui/material";
 import projectData from "../../data/projectData";
 import Project from "./Project";
+import { styled } from "@mui/material/styles";
+
+const Background = styled(Box)({
+  background: "linear-gradient(180deg, #F9FAFB 0%, #EFF1F3 100%)",
+});
 
 const Projects = () => {
   useEffect(() => {
@@ -9,20 +14,33 @@ const Projects = () => {
   }, []);
 
   return (
-    <main>
-      <Container maxWidth="lg" sx={{ py: 6, marginTop: "11vh" }}>
+    <Background>
+      <Container maxWidth="lg" sx={{ py: 6 }}>
         <Typography
-          variant="h3"
+          variant="h2"
           component="h1"
           sx={{
             fontWeight: 700,
-            textAlign: "center",
-            mb: 4,
+            mb: 2,
             letterSpacing: "0.05em",
+            textAlign: "center",
             color: "#1C1C1C",
           }}
         >
           Projects
+        </Typography>
+        <Typography
+          variant="body1"
+          component="p"
+          sx={{
+            mb: 4,
+            fontWeight: 400,
+            letterSpacing: "0.05em",
+            textAlign: "center",
+            color: "#4F4F4F",
+          }}
+        >
+          Check out some of my recent projects below.
         </Typography>
         <Grid container spacing={4}>
           {projectData.map((project) => (
@@ -30,7 +48,7 @@ const Projects = () => {
           ))}
         </Grid>
       </Container>
-    </main>
+    </Background>
   );
 };
 
